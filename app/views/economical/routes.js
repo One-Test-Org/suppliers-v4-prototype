9f430aebb6241f30b8ae5e137870f4b8d78bf016
Nikolaos-Gkionis <nikolaos.gkionis@nhsbsa.nhs.uk>
2023-11-22 23:00:00 +0000

    Financial new journey

--- app/views/economical/routes.js
+++ app/views/economical/routes.js
@@ -10,18 +10,34 @@ router.post('/financial-question', function (req, res) {
     let financialStandIn = req.session.data.financialStandIn;
 
     if (financialStandIn == 'A copy of the most recent two financial years accounts') {
-        res.redirect('/economical/two-or-more');
+        res.redirect('/economical/two-or-more-type');
 
     } else if (financialStandIn == 'A copy of the most recent financial year accounts') {
-        res.redirect('/economical/one-doc');
+        res.redirect('/economical/one-doc-type');
 
     } else if (financialStandIn == 'A copy of the most recent accounts or other information') {
-        res.redirect('/economical/two-or-more');
+        res.redirect('/economical/two-or-more-type');
 
     } else {
         res.redirect('check-answers');
     }
 
+})
+
+router.post('/two-or-more-type', function (req, res) {
+    res.redirect('two-or-more-date');
+})
+
+router.post('/one-doc-type', function (req, res) {
+    res.redirect('one-doc-date');
+})
+
+router.post('/two-or-more-date', function (req, res) {
+    res.redirect('two-or-more');
+})
+
+router.post('/one-doc-date', function (req, res) {
+    res.redirect('one-doc');
 })
 
 router.post('/two-or-more', function (req, res) {

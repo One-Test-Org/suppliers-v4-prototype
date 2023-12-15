72e59ca0257606f2575655ee116590a4ff0f34da
Nikolaos-Gkionis <nikolaos.gkionis@nhsbsa.nhs.uk>
2023-12-15 10:35:50 +0000

    RS-428

--- app/views/mandatory/routes.js
+++ app/views/mandatory/routes.js
@@ -15,6 +15,8 @@ router.post('/exclusion-grounds', function (req, res) {
 
     if (exclusionMan == 'Theft, fraud, bribery etc') {
         res.redirect('theft-fraud-bribery');
+    } else if (exclusionMan == 'Labour market, slavery and human trafficking offences') {
+        res.redirect('labour-market');
     } else if (exclusionMan == '' && startQuestion == 'Individual') {
         res.redirect('/suppliers-d/account-home');
     } else {

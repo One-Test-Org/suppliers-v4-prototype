2f6649de140abf3596dfb5da57374c4dac04f8c2
Nikolaos-Gkionis <nikolaos.gkionis@nhsbsa.nhs.uk>
2023-12-20 12:19:52 +0000

    RS-294

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,78 @@
 const express = require('express');
 const session = require('express-session');
 const router = express.Router();
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+
+router.post('/select-company', async (req, res) => {
+  const apiKey = '4df276be-67a3-4c06-a326-8ca994dc8018';  // Replace with your actual REST API key
+  const companyNumber = req.body.companyNumber;
+
+  try {
+    const companyResponse = await axios.get(`https://api.company-information.service.gov.uk/company/${companyNumber}`, {
+      headers: {
+        Authorization: apiKey,
+      },
+    });
+
+    let officersResponse;
+    try {
+      officersResponse = await axios.get(`https://api.company-information.service.gov.uk/company/${companyNumber}/officers`, {
+        headers: {
+          Authorization: apiKey,
+        },
+      });
+    } catch (error) {
+      console.error('Error fetching officers data:', error.message);
+      officersResponse = { data: null };
+    }
+
+    const responseData = {
+      company: companyResponse.data,
+      officers: officersResponse.data,
+    };
+
+    // Write the response data to a JSON file
+    fs.writeFile('response.json', JSON.stringify(responseData, null, 2), (err) => {
+      if (err) {
+        console.error('Error writing file:', err);
+      } else {
+        console.log('Successfully wrote response data to response.json');
+      }
+    });
+
+    // Modify the structure of the data being passed to the template
+    const templateData = {
+      company: {
+        ...responseData.company,
+        officers: responseData.officers,
+      },
+    };
+
+    res.render('select-company', { data: templateData });
+  } catch (error) {
+    console.error('Error:', error.message);
+    if (error.response) {
+      console.error('Error Response Data:', error.response.data);
+      console.error('Error Response Status:', error.response.status);
+      console.error('Error Response Headers:', error.response.headers);
+    } else if (error.request) {
+      console.error('Error Request:', error.request);
+    } else {
+      console.error('Error', error.message);
+    }
+    res.status(500).send('Error fetching or processing data from Companies House API');
+  }
+});
+
+router.post('/select-company', function (req, res) {
+  res.redirect('start-page');
+});
 
 // allow access to the prototype kit assets
 router.use('/public', express.static('public'));
@@ -56,5 +127,15 @@ router.get('/', function (req, res) {
 })
 
 
+/* Companies House API
 
+import { createApiClient } from "@companieshouse/api-sdk-node";
+
+(async () => {
+  const api = createApiClient("5qSi6ltSkx3Z5tCubLLOpTHB8huQsG9PzhB9s44a1ws");
+  const profile = await api.companyProfile.getCompanyProfile("00006400");
+
+  console.log(profile);
+})()
+*/
 module.exports = router;
